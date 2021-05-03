423f72d48b6fa0ac823e5b84b983fa629cb809da
seominjea1942 <seominjea1942@gmail.com>
2021-05-02 22:26:06 -0700

    second

--- react_heroku/app.js
+++ react_heroku/app.js
@@ -24,6 +24,21 @@ if (!TRANSLATE_API_KEY) {
   throw new Error('AZURE_TRANSLATE_API_KEY is not set.')
 }
 
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000'​, 'http://localhost:5000'​, 'https://shrouded-journey-38552.heroku...​]
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
 
 
 ///////// getNews
@@ -199,6 +214,17 @@ app.get('/translate', (req, res) => {
   .then(data=>res.send(data))
   .catch(err=>console.log(err))
 })
+
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
